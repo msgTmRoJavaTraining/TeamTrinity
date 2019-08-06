a30f455422fe639c7e7d2200359e7619366bfb17
leucul <larisa.leucuta@yahoo.com>
2019-08-06 16:49:06 +0300

    commit

--- src/main/java/beans/LoginBean.java
+++ src/main/java/beans/LoginBean.java
@@ -4,20 +4,20 @@ import lombok.Data;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
-import javax.inject.Named;
 import java.io.Serializable;
 
 @Data
-@Named
 @ApplicationScoped
+@ManagedBean(name="loginBean")
 public class LoginBean implements Serializable {
     private String username;
     private String password;
 
     public String performLogin() {
         if(username.equals("username") && password.equals("password")) {
-            return "";
+            return "permissionManagement";
         } else{
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Date incorecte", "Email sau parola incorecte."));
             return "";
