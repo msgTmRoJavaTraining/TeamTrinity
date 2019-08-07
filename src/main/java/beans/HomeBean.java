bf12ed46a79f9b43c16b6b8b4e100ad71e8197ad
Andrei Astanei <andreihao@gmail.com>
2019-08-07 11:07:20 +0300

    Login Ziua 3

--- src/main/java/beans/HomeBean.java
+++ src/main/java/beans/HomeBean.java
@@ -1,14 +1,30 @@
 package beans;
 
+import entities.Right;
+import entities.Role;
+import entities.User;
 import lombok.Getter;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
+import javax.inject.Inject;
 import java.io.Serializable;
+import java.util.List;
+import java.util.logging.Logger;
 
-@Getter
 @ManagedBean(name = "homeBean")
 @ApplicationScoped
 public class HomeBean implements Serializable {
+    @Inject
+    private LoginBean loginBean;
+
     private boolean isAdmin = false;    //Facem sa dispara butonul de Rights Management
+    private User loggedInUser = loginBean.getLoggedInUser();
+    private List<Role> userRoles = loggedInUser.getRoles();
+
+    @PostConstruct
+    public void init() {
+        System.out.println("USER NAME IS: " + loggedInUser.getName());
+    }
 }
--- src/main/java/beans/LoginBean.java
+++ src/main/java/beans/LoginBean.java
@@ -10,6 +10,7 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 @ManagedBean(name = "loginBean")
@@ -17,6 +18,32 @@ import java.util.List;
 public class LoginBean implements Serializable {
     private String username;
     private String password;
+    private User loggedInUser = new User();
+
+    @PostConstruct
+    public void init() {
+        List<Right> rights = new ArrayList<>();
+        rights.add(new Right("RIGHT_MANAGEMENT"));
+
+        List<Role> roles = new ArrayList<>();
+        Role role = new Role();
+        role.setId(1);
+        role.setRights(rights);
+        role.setRoleName("ADMINISTRATOR");
+        role.setUser(null);
+
+        roles.add(role);
+        
+        loggedInUser.setActive(false);
+        loggedInUser.setAssignedBuggs(null);
+        loggedInUser.setEmail("user@mail.msg.group.com");
+        loggedInUser.setId(1);
+        loggedInUser.setRoles(roles);
+        loggedInUser.setName("FirstName LastName");
+        loggedInUser.setNotifications(null);
+        loggedInUser.setPhoneNumber("12397234");
+        loggedInUser.setUserLogin(null);
+    }
 
     public String performLogin() {
         if (username.equals("username") && password.equals("password")) {
@@ -41,5 +68,24 @@ public class LoginBean implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+//    private String executeLogin(String username, String password) {
+//        loggedInUser = loginEJB.login(username, password);
+//        if(loggedInUser != null) {
+//            if(loggedInUser.isActive() == false) {
+//                loggedInUser.setActive(true);
+//            }
+//
+//            return "homepage";
+//        } else {
+//            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Login failed", "There was a problem with the login problem."));
+//            return "";
+//        }
+//    }
+
+
+    public User getLoggedInUser() {
+        return loggedInUser;
     }
 }
