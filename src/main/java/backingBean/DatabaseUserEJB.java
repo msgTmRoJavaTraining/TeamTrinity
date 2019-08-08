3e0011f9dd595ad6c3444b2c39234df8a772a8c5
Andrei Astanei <andreihao@gmail.com>
2019-08-08 11:48:39 +0300

    Commit pentru adaugare utilizator

--- src/main/java/backingBean/DatabaseUserEJB.java
+++ src/main/java/backingBean/DatabaseUserEJB.java
@@ -1,5 +1,6 @@
 package backingBean;
 
+import entities.Right;
 import entities.Role;
 import entities.User;
 import entities.UserLogin;
@@ -8,7 +9,10 @@ import validators.HashingText;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 @Stateless
@@ -17,7 +21,46 @@ public class DatabaseUserEJB implements Serializable {
     @PersistenceContext(name = "java.training")
     private EntityManager entityManager;
 
-    public void createUser(String firstName,String lastName, String email, String phoneNumber, List<Role> roles,String password) {
+    public List<Role> getSystemRoles() {
+        return entityManager.createQuery("select role.roleName from Role role", Role.class).getResultList();
+    }
+
+    public Role getRoleByString(String roleName) {
+        TypedQuery<Role> query = entityManager.createQuery("select role from Role role where role.roleName = :searchedForName", Role.class);
+        query.setParameter("searchedForName", roleName);
+
+        return query.getSingleResult();
+    }
+
+    //Pentru update roluri ...
+//    public void updateSelectedRoleRights(List<Role> selectedUserRoles, int userId) {
+//        User user = entityManager.find(User.class, userId);
+//        List<Role> newUserRoles = selectedUserRoles;
+//
+//        user.setRoles(newUserRoles);
+//
+//        for(Role r : newUserRoles) {
+//            List<User> existingUserRoles = r.getUser();
+//
+//            if (!existingUserRoles.contains(user)) {
+//                existingUserRoles.add(user);
+//            }
+//        }
+//
+//        entityManager.merge(user);
+//
+//        for(Role r : newUserRoles) {
+//            entityManager.merge(r);
+//        }
+//    }
+
+    public void createUser(String firstName,String lastName, String email, String phoneNumber, List<String> selectedRoles_Strings,String password) {
+        List<Role> selectedRoles_Role = new ArrayList<>();
+        for(String selectedRole : selectedRoles_Strings) {
+            Role selectedRole_Role = getRoleByString(selectedRole);
+            selectedRoles_Role.add(selectedRole_Role);
+        }
+
         User user = new User();
         UserLogin userLogin = new UserLogin();
         userLogin.setPassword(HashingText.getMd5(password));
@@ -25,7 +68,7 @@ public class DatabaseUserEJB implements Serializable {
         user.setName(firstName + lastName);
         user.setEmail(email);
         user.setPhoneNumber(phoneNumber);
-        user.setRoles(roles);
+        user.setRoles(selectedRoles_Role);
         user.setActive(true);
         user.setUserLogin(userLogin);
         entityManager.persist(user);
@@ -52,8 +95,4 @@ public class DatabaseUserEJB implements Serializable {
         //will be completed wednesday 08.08.2019
         return null;
     }
-
-
-
-
 }
--- src/main/java/backingBean/UserBackingBean.java
+++ src/main/java/backingBean/UserBackingBean.java
@@ -1,54 +1,63 @@
 package backingBean;
 
 
+import entities.Right;
 import entities.Role;
+import entities.User;
 import lombok.Data;
 import validators.UserValidator;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
-import javax.inject.Named;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @ManagedBean(name = "userBackingBean")
 @ApplicationScoped
 @Data
 public class UserBackingBean {
+    @Inject
+    private DatabaseUserEJB userEJB;
 
     private String firstName;
     private String lastName;
     private String phoneNumber;
 
     private String email;
-    private List<Role> roles;
+    private List<String> selectedRoles_Strings;
     private String password;
 
-    @Inject
-    private DatabaseUserEJB userEJB;
+    private List<Role> roles;
+    private List<Role> systemRoles;
+    private List<Role> selectedRoles;
 
-        public void createUser() {
+    @PostConstruct
+    public void init() {
+        systemRoles = new ArrayList<>();
 
-        if (UserValidator.isValidEmail(email) && UserValidator.isValidPhoneNumber(phoneNumber)){
-            userEJB.createUser(firstName, lastName, email, phoneNumber, roles, password);
-    }else{
+        systemRoles = userEJB.getSystemRoles();
+    }
+
+    public void createUser() {
+        if (UserValidator.isValidEmail(email) && UserValidator.isValidPhoneNumber(phoneNumber)) {
+            userEJB.createUser(firstName, lastName, email, phoneNumber, selectedRoles_Strings, password);
+        } else {
             FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Date incorecte", "Email invalid"));
         }
     }
 
-    public void updateUser(){}
-
-    public void deleteUser(){
-        userEJB.deleteUser(firstName,lastName);
+    public void updateUser() {
     }
 
-    public void readUser(){
-        userEJB.readUser(firstName,lastName);
+    public void deleteUser() {
+        userEJB.deleteUser(firstName, lastName);
     }
 
-
+    public void readUser() {
+        userEJB.readUser(firstName, lastName);
+    }
 }
